3f462ba0bde99d404f764d58f34a6d18f98ab657
adamberman <adamberman13@gmail.com>
2014-11-25 10:25:03 -0800

    switch to pg, setup

---
+++ app/assets/javascripts/tv_bn_b.js
@@ -0,0 +1,13 @@
+window.TVBnB = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  TVBnB.initialize();
+});
