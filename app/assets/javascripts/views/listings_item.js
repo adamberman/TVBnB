08f6b788bc92339c4e298cbdfa9da24befc0f2a2
adamberman <adamberman13@gmail.com>
2014-11-26 10:19:58 -0800

    add model to listings_item view

--- app/assets/javascripts/views/listings_item.js
+++ app/assets/javascripts/views/listings_item.js
@@ -2,7 +2,7 @@ TVBnB.Views.ListingsItem = Backbone.View.extend({
 	className: 'listings-item',
 	template: JST['index/listings_item'],
 	render: function() {
-		var content = this.template();
+		var content = this.template({listing: this.model});
 		this.$el.html(content);
 		return this;
 	}
