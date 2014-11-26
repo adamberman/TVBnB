a2d35326c75cb9cc81999248a8ad107742914b45
adamberman <adamberman13@gmail.com>
2014-11-26 09:02:41 -0800

    google maps now shows up

---
+++ app/assets/javascripts/views/google.js
@@ -0,0 +1,24 @@
+TVBnB.Views.Google = Backbone.View.extend({
+	initialize: function(){
+		this.mapOptions = {
+			center: new google.maps.LatLng(-34.397, 150.644),
+			zoom: 8
+		};
+	},
+	template: JST['index/google'],
+	onRender: function(){
+		Backbone.CompositeView.prototype.onRender.call(this);
+		this.loadScript();
+	},
+	createMap: function(){
+		this.map = new google.maps.Map(this.$("#map-canvas")[0], this.mapOptions);
+	},
+	render: function(){
+		var content = this.template();
+		this.$el.html(content);
+		setTimeout(function(){
+			this.createMap();
+		}.bind(this), 0)
+		return this;
+	}
+})
--- app/assets/javascripts/views/main.js
+++ app/assets/javascripts/views/main.js
@@ -1,8 +1,16 @@
 TVBnB.Views.Main = Backbone.CompositeView.extend({
+	initialize: function(){
+		this.addGoogle();
+	},
 	template: JST['index/main'],
+	addGoogle: function(){
+		var google = new TVBnB.Views.Google();
+		this.addSubview(".google", google);
+	},
 	render: function() {
 		var content = this.template();
 		this.$el.html(content);
+		this.attachSubviews();
 		return this;
 	}
 })
