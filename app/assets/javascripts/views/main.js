1fb116665b2de1bb197d67294873d620139f71e3
adamberman <adamberman13@gmail.com>
2014-11-26 09:13:04 -0800

    debug divs

--- app/assets/javascripts/views/main.js
+++ app/assets/javascripts/views/main.js
@@ -2,6 +2,7 @@ TVBnB.Views.Main = Backbone.CompositeView.extend({
 	initialize: function(){
 		this.addGoogle();
 	},
+	className: "main",
 	template: JST['index/main'],
 	addGoogle: function(){
 		var google = new TVBnB.Views.Google();
