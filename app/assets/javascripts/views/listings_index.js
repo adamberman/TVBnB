6ecab13a80b5c4f9d374a12ab7620806f41c66b9
adamberman <adamberman13@gmail.com>
2014-11-26 09:55:21 -0800

    page width is now dynamic

--- app/assets/javascripts/views/listings_index.js
+++ app/assets/javascripts/views/listings_index.js
@@ -1,6 +1,6 @@
 TVBnB.Views.ListingsIndex = Backbone.CompositeView.extend({
 	className: 'listings-index',
-	template['index/listings_index'],
+	template: JST['index/listings_index'],
 	render: function() {
 		var content = this.template();
 		this.$el.html(content);
