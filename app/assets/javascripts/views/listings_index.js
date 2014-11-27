f3ec25e7cc8d8ee5c8cf332392cf3f66fe1b8ecb
adamberman <adamberman13@gmail.com>
2014-11-26 23:16:46 -0800

    remove listeners for add and remove on the collection since the collection will only render and subviews should only be added or removed to and from the view upon a movement in the view

--- app/assets/javascripts/views/listings_index.js
+++ app/assets/javascripts/views/listings_index.js
@@ -1,11 +1,7 @@
 TVBnB.Views.ListingsIndex = Backbone.CompositeView.extend({
 	initialize: function(){
-		this.listenTo(this.collection, 'sync', this.render);
-		this.listenTo(this.collection, 'add', this.addListing);
-		this.listenTo(this.collection, 'remove', this.removeListing);
 		this.listenTo(this.collection, 'newSearch', this.filterCollection);
 		this.listenTo(this.collection, 'filter', this.renderListingsSource);
-		this.collection.each(this.addListing.bind(this));
 	},
 	addListing: function(listing){
 		var listing = new TVBnB.Views.ListingsItem({
@@ -18,8 +14,8 @@ TVBnB.Views.ListingsIndex = Backbone.CompositeView.extend({
 		this.collection.trigger('filter');
 	},
 	renderListingsSource: function(){
-		debugger;
-		// this._listingSource.each(this.addListing.bind(this)); //problematic
+		this._listingsSource.each(this.addListing.bind(this));
+		this.render();
 	},
 	removeListing: function(list){
 		var listing = _.find(this.subviews('.listings'), function(subview){
