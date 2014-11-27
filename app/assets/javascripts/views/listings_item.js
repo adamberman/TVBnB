e4bef0cf530deddcd921dfe3cf50f0a92a435d70
adamberman <adamberman13@gmail.com>
2014-11-27 10:28:31 -0800

    turn listings item view into composite view to allow for carousel subview

--- app/assets/javascripts/views/listings_item.js
+++ app/assets/javascripts/views/listings_item.js
@@ -1,9 +1,19 @@
-TVBnB.Views.ListingsItem = Backbone.View.extend({
+TVBnB.Views.ListingsItem = Backbone.CompositeView.extend({
 	className: 'listings-item row panel-default',
 	template: JST['index/listings_item'],
+	initialize: function(){
+		this.addCarousel();
+	},
+	addCarousel: function(){
+		var carousel = new TVBnB.Views.Carousel({
+			model: this.model
+		});
+		this.addSubview('.carousel-container', carousel);
+	},
 	render: function() {
 		var content = this.template({listing: this.model});
 		this.$el.html(content);
+		this.attachSubviews();
 		return this;
 	}
 })
