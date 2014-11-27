a019ecd23b9b4bb743e898af936883b7432e7d2b
adamberman <adamberman13@gmail.com>
2014-11-27 13:13:18 -0800

    debug problem where for some reason not all subviews would be removed in order to be re-rendered so there would be duplicates

--- app/assets/javascripts/views/listings_index.js
+++ app/assets/javascripts/views/listings_index.js
@@ -1,34 +1,37 @@
 TVBnB.Views.ListingsIndex = Backbone.CompositeView.extend({
+	
 	initialize: function(){
 		this.listenTo(this.collection, 'newSearch', this.filterCollection);
 		this.listenTo(this.collection, 'filter', this.renderListingsSource);
+		this.children = [];
 	},
+
 	addListing: function(listing){
-		var listing = new TVBnB.Views.ListingsItem({
+		var subview = new TVBnB.Views.ListingsItem({
 			model: listing 
 		});
-		this.addSubview('.listings', listing);
+		this.children.push(subview);
+		this.addSubview('.listings', subview);
 	},
+
 	filterCollection: function(options){
 		var that = this;
-		this.subviews('.listings').forEach(function(subview){
-			that.removeSubview('.listings', subview);
-		})
+		this.children.forEach(function(listing){
+			that.removeSubview('.listings', listing)
+		});
 		this._listingsSource = this.collection.search(options);
 		this.collection.trigger('filter');
 	},
+
 	renderListingsSource: function(){
 		this._listingsSource.each(this.addListing.bind(this));
 		this.render();
 	},
-	removeListing: function(list){
-		var listing = _.find(this.subviews('.listings'), function(subview){
-			return subview.model === list;
-		});
-		this.removeSubview('.listings', listing);
-	},
+
 	className: 'listings-index row',
+
 	template: JST['index/listings_index'],
+
 	render: function() {
 		var content = this.template();
 		this.$el.html(content);
