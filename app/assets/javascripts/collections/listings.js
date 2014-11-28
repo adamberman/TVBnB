1ea8a37da98d28a92c5b927f0a2148e50ac7b66f
adamberman <adamberman13@gmail.com>
2014-11-27 23:17:50 -0800

    revised method, but still problematic

--- app/assets/javascripts/collections/listings.js
+++ app/assets/javascripts/collections/listings.js
@@ -16,21 +16,34 @@ TVBnB.Collections.Listings = Backbone.Collection.extend({
 		}
 		return listing;
 	},
-	search: function(boundaries, price) {
+	search: function(boundaries, price, date){
+		this._searchD(this._searchBP(boundaries, price), date);
+	},
+
+	_searchBP: function(boundaries, price) {
 		return _(this.filter(function(model){
 			return model.get('latitude') < boundaries.north &&
 				model.get('latitude') > boundaries.south &&
 				model.get('longitude') < boundaries.east &&
 				model.get('longitude') > boundaries.west &&
 				model.get('price') <= price.max &&
-				model.get('price') >= price.min &&
-				for(var i = 0; i < model.get('reservations').length; i++){
-					var start = model.get('reservations')[i][0];
-					var end = model.get('reservations')[i][1];
-					(date.start >= start && date.start <= end) ||
-					(date.end >= start && date.start <= end) ||
-					(date.start <= start && date.end >= end);
-				};
+				model.get('price') >= price.min;
 		}));		
+	},
+
+	_searchD: function(collection, date) {
+		return _(collection.filter(function(model){
+			var overlap = false;
+			for(var i = 0; i < model.get('reservations').length; i++){
+				var start = model.get('reservations')[i][0];
+				var end = model.get('reservations')[i][1];
+				if((date.start >= start && date.start <= end) ||
+					(date.end >= start && date.start <= end) ||
+					(date.start <= start && date.end >= end)){
+					overlap = true;
+				}
+			}
+			return !overlap;
+		}));
 	}
 });
