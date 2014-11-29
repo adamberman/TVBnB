8b8334bcf89025b656b77443e15a7056c30a1da6
adamberman <adamberman13@gmail.com>
2014-11-29 13:36:21 -0800

    add ensureCarousel method to show carousel view

--- app/assets/javascripts/views/listing_show.js
+++ app/assets/javascripts/views/listing_show.js
@@ -1,15 +1,33 @@
 TVBnB.Views.ListingShow = Backbone.CompositeView.extend({
 	initialize: function(){
+		this.listenTo(this.model, 'sync', this.ensureCarousel);
 		this.listenTo(this.model, 'sync', this.render);
+		this._subviews = [];
 	},
 
 	template: JST['show/main'],
 
 	className: 'show-main',
 
+	ensureCarousel: function(){
+		var carousel = new TVBnB.Views.ShowCarousel({
+			model: this.model
+		});
+		var hasCarousel = false;
+		for(var i = 0; i < this._subviews.length; i++){
+			if(this._subviews[i] == carousel){
+				hasCarousel = true;
+			}
+		}
+		if(!hasCarousel){
+			this.addSubview(".large-carousel", carousel);
+		}
+	},
+
 	render: function(){
 		var content = this.template();
 		this.$el.html(content);
+		this.attachSubviews();
 		return this;
 	}
 })
--- app/assets/javascripts/views/show_carousel.js
+++ app/assets/javascripts/views/show_carousel.js
@@ -1,11 +1,20 @@
 TVBnB.Views.ShowCarousel = Backbone.View.extend({
 	template: JST['show/carousel'],
 
+	initCarousel: function(){
+		this.$('.show-carousel').slick({
+			arrows: true,
+			dots: true,
+			speed: 600
+		});
+	},
+
 	render: function(){
-		var content = template({
+		var content = this.template({
 			listing: this.model
 		});
 		this.$el.html(content);
+		this.initCarousel();
 		return this;
 	}
 })
