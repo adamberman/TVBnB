9a45b2211dd60a736a848f03522d76c80ce5730c
adamberman <adamberman13@gmail.com>
2014-11-29 12:48:53 -0800

    add divs to listing show template in order to divide the page into discrete sections

--- app/assets/javascripts/views/listing_show.js
+++ app/assets/javascripts/views/listing_show.js
@@ -6,10 +6,9 @@ TVBnB.Views.ListingShow = Backbone.CompositeView.extend({
 	template: JST['show/main'],
 
 	className: 'show-main',
-	
+
 	render: function(){
-		var content = this.template({listing: this.model});
-		debugger;
+		var content = this.template();
 		this.$el.html(content);
 		return this;
 	}
