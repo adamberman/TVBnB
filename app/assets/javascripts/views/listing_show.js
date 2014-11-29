a07a7c04188c606fe0206588b0a18e6d9c73e3a9
adamberman <adamberman13@gmail.com>
2014-11-29 12:43:49 -0800

    debug listing show

--- app/assets/javascripts/views/listing_show.js
+++ app/assets/javascripts/views/listing_show.js
@@ -1,7 +1,15 @@
 TVBnB.Views.ListingShow = Backbone.CompositeView.extend({
+	initialize: function(){
+		this.listenTo(this.model, 'sync', this.render);
+	},
+
 	template: JST['show/main'],
+
+	className: 'show-main',
+	
 	render: function(){
 		var content = this.template({listing: this.model});
+		debugger;
 		this.$el.html(content);
 		return this;
 	}
