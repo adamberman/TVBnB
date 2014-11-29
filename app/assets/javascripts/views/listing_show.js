63624fc9d291a907cc04f3f2e865ced6e6694f68
adamberman <adamberman13@gmail.com>
2014-11-29 12:37:58 -0800

    pass model as listing to composite view

--- app/assets/javascripts/views/listing_show.js
+++ app/assets/javascripts/views/listing_show.js
@@ -1,7 +1,7 @@
 TVBnB.Views.ListingShow = Backbone.CompositeView.extend({
 	template: JST['show/main'],
 	render: function(){
-		var content = this.template();
+		var content = this.template({listing: this.model});
 		this.$el.html(content);
 		return this;
 	}
