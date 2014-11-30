aadf1987d3b38587f5d6ab2849b2acd28848d966
adamberman <adamberman13@gmail.com>
2014-11-29 16:46:17 -0800

    create reservations collection

---
+++ app/assets/javascripts/collections/reservations.js
@@ -0,0 +1,21 @@
+TVBnB.Collections.Reservations = Backbone.Collection.extend({
+	model: TVBnB.Models.Reservation,
+
+	url: "/api/reservations",
+
+	getOrFetch: function(id){
+		var reservation = this.get(id)
+		var reservations = this;
+		if (!reservation) {
+			reservation = new TVBnB.Models.Reservation({id: id});
+			reservation.fetch({
+				success: function(){
+					reservations.add(reservation);
+				}
+			});
+		} else {
+			reservation.fetch();
+		}
+		return reservation;
+	},
+})
