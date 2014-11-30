458fd8784846f72ea60948dc70e53917e621187d
adamberman <adamberman13@gmail.com>
2014-11-29 16:43:23 -0800

    parse and return reservations as seperate collection

--- app/assets/javascripts/models/listing.js
+++ app/assets/javascripts/models/listing.js
@@ -1,3 +1,17 @@
 TVBnB.Models.Listing = Backbone.Model.extend({
 	urlRoot: "/api/listings"
+	reservations: function(){
+		if(!this._reservations){
+			this._reservations = new TVBnB.Collections.Reservations([], { listing: this });
+		}
+		return this._reservations;
+	},
+
+	parse: function(response){
+		if(response.reservations){
+			this.reservations().set(response.reservations, { parse: true });
+			delete response.reservations;
+		}
+		return response;
+	}
 })
