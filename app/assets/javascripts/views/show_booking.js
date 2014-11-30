4e7adf88f4b41ca31b81ae2e5c0b4c57f0b3ad20
adamberman <adamberman13@gmail.com>
2014-11-29 19:12:19 -0800

    add initDatePicker method to booking form

--- app/assets/javascripts/views/show_booking.js
+++ app/assets/javascripts/views/show_booking.js
@@ -3,11 +3,39 @@ TVBnB.Views.ShowBooking = Backbone.View.extend({
 
 	className: 'booking-container',
 
+	events: {
+		"change form": "changeForm"
+	},
+
+	changeForm: function(event){
+		event.preventDefault();
+		var formValues = $(event.currentTarget).serializeJSON();
+		debugger;
+	},
+
+	initDatePicker: function(){
+		var that = this;
+		setTimeout(function(){
+			$startDate = $('#start').datepicker({
+				startDate: new Date(),
+				autoclose: true
+			});
+			$endDate = $('#end').datepicker({
+				startDate: new Date(),
+				autoclose: true
+			});
+			$startDate.on('changeDate', function(){
+				$endDate.datepicker('setStartDate', $startDate.datepicker('getDate'))
+			});
+		}, 0);
+	},
+
 	render: function(){
 		var content = this.template({
 			listing: this.model
 		});
 		this.$el.html(content);
+		this.initDatePicker();
 		return this;
 	}
 })
