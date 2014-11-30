4bbbf8d049136d97640e1c853b9ef2fa99e69d2a
adamberman <adamberman13@gmail.com>
2014-11-29 16:04:03 -0800

    create show_description view

---
+++ app/assets/javascripts/views/show_description.js
@@ -0,0 +1,13 @@
+TVBnB.Views.ShowDescription = Backbone.View.extend({
+	template: JST['show/description'],
+
+	className: "description-container",
+
+	render: function(){
+		var content = this.template({
+			listing: this.model
+		});
+		this.$el.html(content);
+		return this;
+	}
+})
