cd89472fe6ec5755bd07cca867419dda6282460e
adamberman <adamberman13@gmail.com>
2014-11-29 16:39:23 -0800

    create reservation create route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
   	resources :listings, only: [:create, :show, :index]
+  	resources :reservations, only: :create
   end
 end
