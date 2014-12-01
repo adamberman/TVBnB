255103faca0c6ec486839587c35019c6c66e431c
adamberman <adamberman13@gmail.com>
2014-12-01 09:57:48 -0800

    restyle and unlink images to allow carousel to work

--- app/assets/javascripts/views/listings_item.js
+++ app/assets/javascripts/views/listings_item.js
@@ -1,5 +1,5 @@
 TVBnB.Views.ListingsItem = Backbone.CompositeView.extend({
-	className: 'listings-item panel-default',
+	className: 'col-6 row-space-1',
 	template: JST['index/listings_item'],
 	initialize: function(){
 		this.addCarousel();
