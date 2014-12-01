465d2f168a28a27feb5a15c039a41d85886407c8
adamberman <adamberman13@gmail.com>
2014-11-30 17:20:41 -0800

    add method to attach reviews index to reviews-container

--- app/assets/javascripts/views/reviews_container.js
+++ app/assets/javascripts/views/reviews_container.js
@@ -1,11 +1,18 @@
 TVBnB.Views.ReviewsContainer = Backbone.CompositeView.extend({
 	initialize: function(){
-
+		this.addReviewsIndex();
 	},
 
 	template: JST['show/reviews_container'],
 
 	className: 'reviews-container',
+
+	addReviewsIndex: function(){
+		var reviews = TVBnB.Views.ReviewsIndex({
+			collection: this.model.reviews()
+		});
+		this.addSubview('.reviews-index', reviews);
+	},
 
 	render: function(){
 		content = this.template();
