9837de7cb0592bb80db4700b55716b465fb5cdbd
adamberman <adamberman13@gmail.com>
2014-11-30 17:30:09 -0800

    finish review-show

--- app/assets/javascripts/views/review_show.js
+++ app/assets/javascripts/views/review_show.js
@@ -1,5 +1,17 @@
 TVBnB.Views.ReviewShow = Backbone.View.extend({
 	initialize: function(){
 		this.listenTo(this.model, 'sync', this.render);
+	},
+
+	template: JST['show/review_show'],
+
+	className: 'review-show',
+
+	render: function(){
+		var content = this.template({
+			review: this.model
+		});
+		this.$el.html(content);
+		return this;
 	}
 })
