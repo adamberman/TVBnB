c8552d84cbcdb5daad57cfa57cbba64f3d6cacff
adamberman <adamberman13@gmail.com>
2014-12-01 10:12:32 -0800

    create opening-main bb view

---
+++ app/assets/javascripts/views/opening_main.js
@@ -0,0 +1,31 @@
+TVBnB.Views.OpeningMain = Backbone.CompositeView.extend({
+	initialize: function(){
+		this.addCarousel();
+		this.addSearch();
+	},
+
+	template: JST['opening/main'],
+
+	className: 'opening-main',
+
+	events: {
+		
+	},
+
+	addCarousel: function(){
+		var carousel = new TVBnB.Views.OpeningCarousel();
+		this.addSubview('.opening-carousel', carousel);
+	},
+
+	addSearch: function(){
+		var search = new TVBnB.Views.OpeningSearch();
+		this.addSubview('opening-search', search);
+	}
+
+	render: function(){
+		var content = this.template();
+		this.$el.html(content);
+		this.attachSubviews();
+		return this;
+	}
+})
