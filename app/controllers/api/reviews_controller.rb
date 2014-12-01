4b2853704f590e0e24872da54fc4036b90cc13b2
adamberman <adamberman13@gmail.com>
2014-11-30 16:06:55 -0800

    create reviews controller

---
+++ app/controllers/api/reviews_controller.rb
@@ -0,0 +1,29 @@
+module Api
+	class ReviewsController < ApplicationController
+		def create
+			@review = current_listing.reviews.new(review_params)
+			@review.user_id = current_user.id
+
+			if @review.save
+				render json: @review
+			else
+				render json: @review.errors.full_messages, status: :unprocessable_entity
+			end
+		end
+
+		private
+
+		def current_listing
+			if params[:id]
+				@review = Review.find(params[:id])
+				@listing = @review.listing
+			elsif params[:review]
+				@listing = Listing.find(params[:review][:listing_id])
+			end
+		end
+
+		def review_params
+			params.require(:review).permit(:listing_id, :content)
+		end		
+	end
+end
