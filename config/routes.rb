12cd195b99967a55e1131986ae28cf61f5f93aec
adamberman <adamberman13@gmail.com>
2014-11-30 18:51:29 -0800

    add routes in order to create a user bb model

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
-  resources :users
+  resources :users, except: :show
   resource :session
 
   namespace :api, defaults: { format: :json } do
   	resources :listings, only: [:create, :show, :index]
   	resources :reservations, only: :create
   	resources :reviews, only: [:create, :show]
+  	resources :users, only: :show
   end
 end
