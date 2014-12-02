c8ca003d96277b7da7f4e888b6e5b92f39be5752
adamberman <adamberman13@gmail.com>
2014-12-01 17:17:32 -0800

    add datepicker, make calendar functional

--- app/assets/javascripts/views/opening_search.js
+++ app/assets/javascripts/views/opening_search.js
@@ -3,9 +3,30 @@ TVBnB.Views.OpeningSearch = Backbone.View.extend({
 
 	template: JST['opening/search'],
 
+	initDatePicker: function(){
+		var that = this;
+		setTimeout(function(){
+			that.$startDate = $('#start').datepicker({
+				startDate: new Date(),
+				autoclose: true
+			});
+			that.$endDate = $('#end').datepicker({
+				startDate: new Date(),
+				autoclose: true
+			});
+			that.$startDate.on('changeDate', function(){
+				that.$endDate.datepicker('setStartDate', that.$startDate.datepicker('getDate'))
+			});
+			that.$endDate.on('changeDate', function(){
+				that.$startDate.datepicker('setEndDate', that.$endDate.datepicker('getDate'))
+			});
+		}, 0);
+	},
+
 	render: function(){
 		var content = this.template();
 		this.$el.html(content);
+		this.initDatePicker();
 		return this;
 	}
 })
