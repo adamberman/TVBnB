fcad2b5fbc8770a0de6d9a018d819d01cffbff7c
adamberman <adamberman13@gmail.com>
2014-12-01 20:13:44 -0800

    geocode addresses in google maps view

--- app/assets/javascripts/views/google.js
+++ app/assets/javascripts/views/google.js
@@ -1,13 +1,11 @@
 TVBnB.Views.Google = Backbone.View.extend({
 	initialize: function(){
+		this.geocoder = new google.maps.Geocoder();
 		this.location = 'new york, new york';
 		if($.cookie('location')){
 			this.location = $.cookie('location');
 		}
-		this.mapOptions = {
-			center: new google.maps.LatLng(37, -122),
-			zoom: 8
-		};
+		this.codeAddress();
 		this.markers = {}
 		this.listenTo(this.collection, 'sync', this.addAllListings);
 		this.listenTo(this.collection, 'add', this.addListing);
@@ -23,6 +21,20 @@ TVBnB.Views.Google = Backbone.View.extend({
 		this.map = new google.maps.Map(this.$("#map-canvas")[0], this.mapOptions);
 		this.addAllListings();
 		google.maps.event.addListener(this.map, 'idle', this.setSearchBounds.bind(this));
+	},
+
+	codeAddress: function(){
+		this.geocoder.geocode( {'address': this.location}, function(results, status){
+			if(status == google.maps.GeocoderStatus.OK) {
+				this.latLng = results[0].geometry.location;
+				this.mapOptions = {
+					center: this.latLng,
+					zoom: 8
+				};
+			} else {
+				alert('Geocode was not successful for the following reason: ' + status)
+			}
+		});
 	},
 
 	setSearchBounds: function(){
