05a866328bfb11eeae76cfbfae0f56552394676b
adamberman <adamberman13@gmail.com>
2014-12-03 09:29:31 -0800

    make create method for listings controller

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -1,6 +1,14 @@
 module Api
 	class ListingsController < ApplicationController
 		def create
+			@listing = current_user.listings.new(listing_params)
+
+			if @listing.save
+				flash.now[:success] = ["You have successfully listed your propert!"]
+				render :show
+			else
+				render json: @listing.errors.full_messages, status: :unprocessable_entity
+			end
 		end
 
 		def show
@@ -11,6 +19,12 @@ module Api
 		def index
 			@listings = Listing.all.includes(:images, :reservations)
 			render :index
+		end
+
+		private
+
+		def listing_params
+			params.require(:listing).permit(:price, :address, :name, :description)
 		end
 	end
 end
