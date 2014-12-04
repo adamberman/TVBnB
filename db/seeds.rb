08995c88eca0900913c1cb3c43ebb20c7741fdc0
adamberman <adamberman13@gmail.com>
2014-12-03 17:02:35 -0800

    make navbar work better

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 ActiveRecord::Base.transaction do
-	User.create!(username: "George", password: "vandalay_industries", gravatar_url: 'george.jpg')
+	User.create!(username: "George", password: "vandalay_industries", gravatar_url: '/assets/george.jpg')
 end
