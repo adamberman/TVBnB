7b8147d690333b2aa56496c13ad28443418f2d2e
adamberman <adamberman13@gmail.com>
2014-12-03 18:12:18 -0800

    add method to save file

--- app/assets/javascripts/views/new_images_form.js
+++ app/assets/javascripts/views/new_images_form.js
@@ -4,9 +4,29 @@ TVBnB.Views.NewImagesForm = Backbone.View.extend({
 		this.listenTo(this.model, 'sync', this.render);
 	},
 
+	events: {
+		'click button.new-image-button': 'submitImage'
+	},
+
 	className: "new-images-form",
 
 	template: JST['new/images_form'],
+
+	submitImage: function(event){
+		event.preventDefault();
+		var image = new TVBnB.Models.Image();
+		var values = {};
+
+		_.each(this.$('form').serializeArray(), function(input){
+			values[input.name] = input.value;
+		})
+
+		image.save(values, {
+			iframe: true,
+			files: this.$('form :file'),
+			data: values
+		});
+	},
 
 	render: function() {
 		var content = this.template({
