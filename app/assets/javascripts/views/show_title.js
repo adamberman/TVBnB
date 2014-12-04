ac1ee34a64b844724039b1aad05408c3dbfa81ba
adamberman <adamberman13@gmail.com>
2014-12-03 21:04:42 -0800

    display user name and image in listing show page

--- app/assets/javascripts/views/show_title.js
+++ app/assets/javascripts/views/show_title.js
@@ -1,11 +1,19 @@
 TVBnB.Views.ShowTitle = Backbone.View.extend({
+
+	initialize: function() {
+		this.user = new TVBnB.Models.User({id: this.model.escape('user_id')});
+		this.user.fetch();
+		this.listenTo(this.user, 'sync', this.render)
+	},
+
 	template: JST['show/title'],
 
 	className: "title-container",
 
 	render: function(){
 		var content = this.template({
-			listing: this.model
+			listing: this.model,
+			user: this.user
 		});
 		this.$el.html(content);
 		return this;
