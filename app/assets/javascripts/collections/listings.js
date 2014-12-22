07818ce597092be1a4ac902008ebad74c82d82ac
adamberman <adamberman13@gmail.com>
2014-12-22 09:48:35 -0800

    comment, refactor listings search methodology

--- app/assets/javascripts/collections/listings.js
+++ app/assets/javascripts/collections/listings.js
@@ -20,22 +20,32 @@ TVBnB.Collections.Listings = Backbone.Collection.extend({
 		return listing;
 	},
 
+	//parent method for calling various search methods, returns sub-collection of listings
 	search: function (boundaries, min_price, max_price, start_date, end_date) {
-		var collection = this._searchBP(boundaries, min_price, max_price)
+		var collection = this._inBoundaries(boundaries);
+		collection = this._inPrice(collection, min_price, max_price);
 		return this._searchD(collection, start_date, end_date);
 	},
 
-	_searchBP: function (boundaries, min_price, max_price) {
+	//checks if the listings in the current collection are within the map boundaries
+	_inBoundaries: function(boundaries) {
 		return _(this.filter(function(model) {
 			return model.get('latitude') < boundaries.north &&
 				model.get('latitude') > boundaries.south &&
 				model.get('longitude') < boundaries.east &&
-				model.get('longitude') > boundaries.west &&
-				model.get('price') <= max_price &&
+				model.get('longitude') > boundaries.west;
+		}));
+	},
+
+	//checks if the listings in the current sub-collection are within the given price range
+	_inPrice: function (collection, min_price, max_price) {
+		return _(collection.filter(function(model) {
+			return model.get('price') <= max_price &&
 				model.get('price') >= min_price;
 		}));		
 	},
 
+	//checks if the listings in the current sub-collection are within the given date range
 	_searchD: function(collection, start_date, end_date) {
 		return _(collection.filter(function(model){
 			var overlap = false;
