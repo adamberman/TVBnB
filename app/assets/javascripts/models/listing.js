79d22324a384dca434ec87ebfd02f0d5d5888d01
adamberman <adamberman13@gmail.com>
2014-12-22 14:56:59 -0800

    comment the listing bb model

--- app/assets/javascripts/models/listing.js
+++ app/assets/javascripts/models/listing.js
@@ -2,6 +2,7 @@ TVBnB.Models.Listing = Backbone.Model.extend({
 	
 	urlRoot: "/api/listings",
 	
+	// sets and returns collection of reservations belonging to the listing
 	reservations: function () {
 		if (!this._reservations) {
 			this._reservations = new TVBnB.Collections.Reservations([], { listing: this });
@@ -9,6 +10,7 @@ TVBnB.Models.Listing = Backbone.Model.extend({
 		return this._reservations;
 	},
 
+	// sets and returns a collection of reviews belonging to the listing
 	reviews: function () {
 		if (!this._reviews) {
 			this._reviews = new TVBnB.Collections.Reviews([], { listing: this });
@@ -16,6 +18,7 @@ TVBnB.Models.Listing = Backbone.Model.extend({
 		return this._reviews;
 	},
 
+	// removes responses and reviews from being a property of the listing and calls the methods setting the data as collections
 	parse: function (response) {
 		if (response.reservations) {
 			this.reservations().set(response.reservations, { parse: true });
